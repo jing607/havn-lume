5d6ab2b8f8f23d7ea73cfa0c6d33a868117ddd23
Haojing XU <leslie.hj.xu.design@gmail.com>
2025-02-11 16:52:21 +0100

    config: update repo name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/havn-lume/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/hex-react-2/' : '/',
   plugins: [react()],
 })
